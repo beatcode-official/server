d5513317405eb2bc8a3588101be6bfd1a8da8e5d
Bao Dang <baochidangg@gmail.com>
2024-11-29 01:17:35 -0500

    Reorganized folders

--- server/app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# run [openssl rand -hex 32] in terminal to generate a secret key, then paste them to .env file
-# we can talk about this later when the database is properly set up
-SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-
-ALGORITHM = "HS256"
-
-ACCESS_TOKEN_EXPIRE_MINUTES = 30

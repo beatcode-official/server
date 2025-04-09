a1eacf00e90bc3b74e1c80bbd84e376c9acd79cc
Bao Dang <baochidangg@gmail.com>
2025-04-08 23:03:00 -0400

    Refactored game

--- app/api/endpoints/game/websockets.py
+++ app/api/endpoints/game/websockets.py
@@ -165,18 +165,35 @@ async def game_websocket(
     current_user: User = Depends(get_current_user_ws),
     db: Session = Depends(get_db),
 ):
-    """Handle active game connections and gameplay interactions."""
+    """
+    WebSocket endpoint for the game
+
+    :param websocket: WebSocket object
+    :param game_id: ID of the game
+    :param current_user: User object
+    :param db: Database session
+    """
     game_state = game_manager.active_games.get(game_id)
 
-    reason = await _is_game_invalid(game_state, current_user.id)
-    if reason:
-        return await websocket.close(code=4000, reason=reason)
+    # Check if the game exists and is not finished
+    if not game_state or game_state.status == GameStatus.FINISHED:
+        return await websocket.close(
+            code=4000, reason="Game not found or already finished"
+        )
 
+    # Check if the user is a player in the game
+    if current_user.id not in [game_state.player1.user_id, game_state.player2.user_id]:
+        return await websocket.close(code=4000, reason="Not a player in this game")
+
+    # Get the player state
     player = game_state.get_player_state(current_user.id)
+    if not player:
+        return await websocket.close(code=4000, reason="Player not found")
+
     old_ws = player.ws
     player.ws = websocket
 
-    # Close the old connection if it exists
+    # Close the old WebSocket connection if it exists
     if old_ws:
         try:
             await old_ws.close(
@@ -187,9 +204,8 @@ async def game_websocket(
             pass
 
     try:
-        # Send initial game state
         game_view = game_manager.create_game_view(game_state, current_user.id)
-        await player.ws.send_json(
+        await websocket.send_json(
             {"type": "game_state", "data": game_view.model_dump()}
         )
 
@@ -201,196 +217,174 @@ async def game_websocket(
             current_problem = ProblemManager.prepare_problem_for_client(
                 game_state.problems[player.current_problem_index]
             )
-            await player.ws.send_json({"type": "problem", "data": current_problem})
+            await websocket.send_json({"type": "problem", "data": current_problem})
 
         # Start the game if both players are connected and the game is waiting
         opponent = game_state.get_opponent_state(current_user.id)
-
         if opponent and opponent.ws and game_state.status == GameStatus.WAITING:
-            await _start_game(game_state)
-        await _run_game_loop(game_state, game_view, player, db)
+            game_state.status = GameStatus.IN_PROGRESS
+
+            problem = ProblemManager.prepare_problem_for_client(game_state.problems[0])
+
+            await game_state.broadcast_event(GameEvent(type="game_start", data={}))
+
+            await game_state.broadcast_event(GameEvent(type="problem", data=problem))
+
+        while True:
+            try:
+                data = await asyncio.wait_for(websocket.receive_json(), timeout=1.0)
+
+                # No longer accept messages if the game is finished
+                if game_state.status == GameStatus.FINISHED:
+                    break
+
+                # Handle chat messages
+                if data["type"] == "chat":
+                    await game_state.broadcast_event(
+                        GameEvent(
+                            type="chat",
+                            data={
+                                "sender": current_user.username,
+                                "message": data["data"]["message"],
+                                "timestamp": time.time(),
+                            },
+                        )
+                    )
+
+                elif data["type"] == "ability":
+                    error = await ability_manager.handle_ability_message(
+                        game_state, game_manager, current_user.id, data["data"]
+                    )
+                    if error:
+                        await websocket.send_json(
+                            {"type": "error", "data": {"message": error}}
+                        )
+
+                elif data["type"] == "query":
+                    await websocket.send_json(
+                        {"type": "game_state", "data": game_view.model_dump()}
+                    )
+                # Handle player forfeits
+                elif data["type"] == "forfeit":
+                    await game_manager.forfeit_game(game_id, current_user.id)
+                    await game_manager.handle_game_end(game_state, db)
+                # Handle player submissions
+                elif data["type"] == "submit":
+                    # Check if the player is submitting too fast
+                    current_time = time.time()
+                    submission_cooldown = (
+                        settings.SUBMISSION_COOLDOWN if not settings.TESTING else 2
+                    )
+                    if (
+                        player.last_submission is not None
+                        and current_time - player.last_submission < submission_cooldown
+                    ):
+                        await player.send_event(
+                            GameEvent(
+                                type="error",
+                                data={
+                                    "message": "You're submitting too fast. Please wait before submitting again"
+                                },
+                            )
+                        )
+                        continue
+
+                    player.last_submission = current_time
+
+                    # Execute the player's code on the hidden test cases
+                    code = data["data"]["code"]
+                    lang = data["data"]["lang"]  # java, cpp, python
+                    problem_index = player.current_problem_index
+                    problem = game_state.problems[problem_index]
+
+                    validation_data = ProblemManager.get_problem_for_validation(problem)
+                    result = await code_execution.execute_code(
+                        code,
+                        validation_data["method_name"],
+                        validation_data["hidden_test_cases"],
+                        validation_data["hidden_test_results"],
+                        validation_data["sample_test_cases"],
+                        validation_data["sample_test_results"],
+                        problem.difficulty,
+                        getattr(validation_data["compare_func"], lang),
+                        lang,
+                    )
+                    result = result.to_dict()
+
+                    # Process the submission result
+                    if result["success"]:
+                        # Fetch deducted HP and whether the problem was solved
+                        submission_result = await game_manager.process_submission(
+                            game_id,
+                            current_user.id,
+                            result["summary"]["passed_tests"],
+                            result["summary"]["total_tests"],
+                        )
+
+                        # Send the submission result to the player
+                        await player.send_event(
+                            GameEvent(
+                                type="submission_result",
+                                data={**result, **submission_result},
+                            )
+                        )
+
+                        # Send the game state to both players
+                        await game_state.player1.send_event(
+                            GameEvent(
+                                type="game_state",
+                                data=game_manager.create_game_view(
+                                    game_state, game_state.player1.user_id
+                                ).model_dump(),
+                            )
+                        )
+
+                        await game_state.player2.send_event(
+                            GameEvent(
+                                type="game_state",
+                                data=game_manager.create_game_view(
+                                    game_state, game_state.player2.user_id
+                                ).model_dump(),
+                            )
+                        )
+
+                        # Send the next problem if the current one was solved
+                        if (
+                            submission_result["problem_solved"]
+                            and problem_index < len(game_state.problems) - 1
+                        ):
+                            next_problem = ProblemManager.prepare_problem_for_client(
+                                game_state.problems[problem_index + 1]
+                            )
+                            await player.send_event(
+                                GameEvent(type="problem", data=next_problem)
+                            )
+
+                        # Check if the game has ended and handle it
+                        if await game_manager.check_game_end(game_id):
+                            await game_manager.handle_game_end(game_state, db)
+                    else:
+                        # Submission failed, send the result to the player
+                        await player.send_event(
+                            GameEvent(type="submission_result", data=result)
+                        )
+
+            except asyncio.TimeoutError:
+                continue
+            except Exception as e:
+                try:
+                    await websocket.send_json(
+                        {
+                            "type": "error",
+                            "data": {"message": "An error occurred: " + str(e)},
+                        }
+                    )
+                except Exception:
+                    raise e
+
     except WebSocketDisconnect:
         pass
     except RuntimeError:
         pass
     except Exception as _:
         print(f"Error in game websocket: {traceback.format_exc()}")
-
-
-async def _is_game_invalid(game_state: GameState | None, current_user_id: int):
-    # Check if the game exists and is not finished
-    if not game_state or game_state.status == GameStatus.FINISHED:
-        return "Game not found or already finished"
-
-    # Check if the user is a player in the game
-    if current_user_id not in [game_state.player1.user_id, game_state.player2.user_id]:
-        return "Not a player in this game"
-
-    # Get the player state and handle reconnection
-    player = game_state.get_player_state(current_user_id)
-    if not player:
-        return "Player not found"
-
-
-async def _start_game(game_state):
-    game_state.status = GameStatus.IN_PROGRESS
-
-    problem = ProblemManager.prepare_problem_for_client(game_state.problems[0])
-
-    await game_state.broadcast_event(GameEvent(type="game_start", data={}))
-    await game_state.broadcast_event(GameEvent(type="problem", data=problem))
-
-
-async def _run_game_loop(game_state, game_view, player, db):
-    while True:
-        try:
-            data = await asyncio.wait_for(player.ws.receive_json(), timeout=1.0)
-            # No longer accept messages if the game is finished
-            if game_state.status == GameStatus.FINISHED:
-                break
-            await _process_message(game_state, game_view, player, db, data)
-        except asyncio.TimeoutError:
-            continue
-        except Exception as e:
-            try:
-                await player.ws.send_json(
-                    {
-                        "type": "error",
-                        "data": {"message": "An error occurred: " + str(e)},
-                    }
-                )
-            except Exception as e:
-                print(f"Failed to send error message: {e}")
-
-
-async def _process_message(game_state, game_view, player, db, data):
-    if data["type"] == "chat":
-        await game_state.broadcast_event(
-            GameEvent(
-                type="chat",
-                data={
-                    "sender": player.username,
-                    "message": data["data"]["message"],
-                    "timestamp": time.time(),
-                },
-            )
-        )
-    elif data["type"] == "ability":
-        error = await ability_manager.handle_ability_message(
-            game_state, game_manager, player.user_id, data["data"]
-        )
-        if error:
-            await player.ws.send_json({"type": "error", "data": {"message": error}})
-    elif data["type"] == "query":
-        await player.ws.send_json(
-            {"type": "game_state", "data": game_view.model_dump()}
-        )
-    elif data["type"] == "forfeit":
-        await game_manager.forfeit_game(game_state.id, player.user_id)
-        await game_manager.handle_game_end(game_state, db)
-    elif data["type"] == "submit":
-        try:
-            await _handle_submit(game_state, player, data, db)
-        except SubmittingTooFastError as e:
-            await player.send_event(GameEvent(type="error", data={"message": str(e)}))
-
-
-async def _handle_submit(game_state, player, data, db):
-    remaining_cooldown = _remaining_cooldown(game_state, player)
-    if remaining_cooldown > 0:
-        raise SubmittingTooFastError(remaining_cooldown)
-    player.last_submission = time.time()
-
-    result = await _execute_code(game_state, player, data)
-    await _process_submission_result(game_state, player, result, db)
-
-
-def _remaining_cooldown(game_state, player):
-    if not player or not player.last_submission:
-        return 0
-    current_time = time.time()
-    submission_cooldown = settings.SUBMISSION_COOLDOWN if not settings.TESTING else 2
-    submission_time_diff = current_time - player.last_submission
-    return submission_cooldown - submission_time_diff
-
-
-async def _execute_code(game_state, player, data):
-    # Execute the player's code on the hidden test cases
-    code = data["data"]["code"]
-    lang = data["data"]["lang"]  # java, cpp, python
-    problem_index = player.current_problem_index
-    problem = game_state.problems[problem_index]
-
-    validation_data = ProblemManager.get_problem_for_validation(problem)
-    result = await code_execution.execute_code(
-        code,
-        validation_data["method_name"],
-        validation_data["hidden_test_cases"],
-        validation_data["hidden_test_results"],
-        validation_data["sample_test_cases"],
-        validation_data["sample_test_results"],
-        problem.difficulty,
-        getattr(validation_data["compare_func"], lang),
-        lang,
-    )
-    return result.to_dict()
-
-
-async def _process_submission_result(game_state, player, result, db):
-    if result["success"]:
-        # Fetch deducted HP and whether the problem was solved
-        submission_result = await game_manager.process_submission(
-            game_state.id,
-            player.user_id,
-            result["summary"]["passed_tests"],
-            result["summary"]["total_tests"],
-        )
-
-        # Send the submission result to the player
-        await player.send_event(
-            GameEvent(
-                type="submission_result",
-                data={**result, **submission_result},
-            )
-        )
-
-        await _broadcast_game_state(game_state)
-        await _send_next_problem(game_state, player, submission_result)
-
-        # Check if the game has ended and handle it
-        if await game_manager.check_game_end(game_state.id):
-            await game_manager.handle_game_end(game_state, db)
-    else:
-        # Submission failed, send the result to the player
-        await player.send_event(GameEvent(type="submission_result", data=result))
-
-
-async def _broadcast_game_state(game_state):
-    await game_state.player1.send_event(
-        GameEvent(
-            type="game_state",
-            data=game_manager.create_game_view(
-                game_state, game_state.player1.user_id
-            ).model_dump(),
-        )
-    )
-    await game_state.player2.send_event(
-        GameEvent(
-            type="game_state",
-            data=game_manager.create_game_view(
-                game_state, game_state.player2.user_id
-            ).model_dump(),
-        )
-    )
-
-
-async def _send_next_problem(game_state, player, submission_result):
-    """Send the next problem to the player if the current one was solved."""
-    if (
-        submission_result["problem_solved"]
-        and player.current_problem_index < len(game_state.problems) - 1
-    ):
-        next_problem = ProblemManager.prepare_problem_for_client(
-            game_state.problems[player.current_problem_index + 1]
-        )
-        await player.send_event(GameEvent(type="problem", data=next_problem))
